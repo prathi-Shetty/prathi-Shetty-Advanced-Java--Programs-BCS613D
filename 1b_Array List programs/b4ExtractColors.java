38050e34eaf37cc0b5fbc156eccec64d3b2b4a74
prathi-Shetty <sprathi630@gmail.com>
2025-05-20 20:02:07 +0530

    Update b4ExtractColors.java

--- 1b_Array List programs/b4ExtractColors.java
+++ 1b_Array List programs/b4ExtractColors.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 public class ExtractColors {
     public static void main(String[] args) {
+        // Create an ArrayList of colors
         ArrayList<String> colors = new ArrayList<>();
         colors.add("Red");
         colors.add("Blue");
@@ -13,10 +14,14 @@ public class ExtractColors {
         colors.add("Yellow");
         colors.add("Purple");
 
+        // Display original list
         System.out.println("Original Colors List: " + colors);
 
+        // Extract 1st and 2nd elements using subList (index 0 to 2, exclusive)
         List<String> subList = colors.subList(0, 2);
 
+        // Display extracted elements
         System.out.println("Extracted Elements (1st and 2nd): " + subList);
     }
 }
+
