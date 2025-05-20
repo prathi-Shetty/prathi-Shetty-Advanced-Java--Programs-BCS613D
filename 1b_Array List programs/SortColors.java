bf9f3610f5c6adc1e70fdd5c4d5504f8881bf067
prathi-Shetty <sprathi630@gmail.com>
2025-05-20 19:42:41 +0530

    Update SortColors.java

--- 1b_Array List programs/SortColors.java
+++ 1b_Array List programs/SortColors.java
@@ -1,4 +1,4 @@
-/* Write a java program for getting different colors through ArrayList interface and sort them using Collections.sort( ArrayListObj)*/
+/* 3 Write a java program for getting different colors through ArrayList interface and sort them using Collections.sort( ArrayListObj)*/
 package listOperations;
 import java.util.*;
 public class SortColors {
